859f9d65ec7fe7ab164a92bb5febdc893fd263d7
Egor Kovalev <ekov999@gmail.com>
2023-01-19 01:52:55 +0300

    Change tb

--- 1_hello_world/testbench.py
+++ 1_hello_world/testbench.py
@@ -12,7 +12,8 @@ class send_hello_world_in_any_time (uvm_test):
 
     async def run_phase(self):
         self.raise_objection()
-        alarm  = random.randrange(1,100,1)
+        alarm  = random.randrange(1000,100000,1)
+        self.logger.info("Message will be started after %d  ns",  alarm)
         await Timer(alarm,'ns')
         self.logger.info('Hello World')
         self.drop_objection()
